8f1d08272dac5f0675261499f2235f8d5a1e948f
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2022-02-14 13:35:53 +0100

    hello.py
    Maak een programmatje genaamd hello.py als dit programmatje start moet het volgende zichtbaar zijn:
    
    Een programmatje met de title Hello
    Je 3 favoriete kleuren
    De tekst Hello tkinter!

--- hello.py
+++ hello.py
@@ -3,19 +3,19 @@ import tkinter
 window = tkinter.Tk()
 window.title('Hello')
 window.configure(bg='black')
-window.geometry("200x200")
+window.geometry("150x100")
 
 # box 1
 box1 = tkinter.Label(
     window,
-    text="Hello Tkinter!",
+    text="Hello \n Tkinter!",
     bg="red",
     fg="white"
 )
 
 box1.pack(
     ipadx=20,
-    ipady=40,
+    ipady=10,
     expand=True
 )
 
